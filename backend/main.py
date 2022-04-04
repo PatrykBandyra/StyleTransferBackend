7a5713bf52b0ec4609cc36489a8b202d45faa88a
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-04-04 16:11:02 +0200

    initial backend with example transformer model

---
+++ backend/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+from fastapi.exceptions import HTTPException
+from io import BytesIO
+from PIL import Image
+import torch
+from torchvision import transforms, utils
+import pystiche
+
+app = FastAPI()
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+transformer = pystiche.demo.transformer().to(device)
+transformer.load_state_dict(torch.load('example_transformer.pth'))
+transformer.eval()
+
+
+def perform_nst(input_image: Image.Image) -> torch.Tensor:
+    image_tensor = transforms.ToTensor()(input_image)
+    if len(list(image_tensor.size())) == 4:
+        image_tensor = image_tensor.unsqueeze(0)
+    image_tensor.to(device)
+
+    with torch.no_grad():
+        output_image_tensor = transformer(image_tensor)
+    return output_image_tensor
+
+
+def transform_to_pillow_image(tensor_image) -> Image.Image:
+    grid = utils.make_grid(tensor_image)
+    ndarr = grid.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+    return Image.fromarray(ndarr)
+
+
+@app.post('/nst')
+async def neural_style_transfer(file: UploadFile = File(...)):
+    if file.content_type not in ['image/jpeg']:
+        raise HTTPException(400, detail='Invalid file type')
+
+    original_image = Image.open(file.file)
+    nst_image = transform_to_pillow_image(perform_nst(original_image))
+
+    response_image = BytesIO()
+    nst_image.save(response_image, 'JPEG')
+    response_image.seek(0)
+
+    return StreamingResponse(response_image, media_type='image/jpeg')
