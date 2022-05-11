37050eec81e829cc80a95ac834d68da9d6d947fd
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-05-11 15:37:47 +0200

    segmentation and nst combined test

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,14 @@ from fastapi.exceptions import HTTPException
 from io import BytesIO
 from PIL import Image
 import contextlib
+import numpy as np
 import os
 import torch
 from torchvision import transforms
 import pystiche
 from pystiche.image.io import import_from_pil, export_to_pil
 import cv2
+import mediapipe as mp
 
 app = FastAPI()
 
@@ -73,13 +75,48 @@ async def neural_style_transfer(file: UploadFile = File(...)):
     return StreamingResponse(response_image, media_type='image/jpeg')
 
 
+mp_drawing = mp.solutions.drawing_utils
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+
+BG_COLOR = (255, 255, 255)
+
+
 @app.post('/nst2/')
 async def test(file: UploadFile = File(...)):
     if file.content_type not in ['image/jpeg']:
         raise HTTPException(400, detail='Invalid file type')
 
-    image = cv2.imread(file.file)
-    print(image.shape)
+    original_image_pillow = Image.open(file.file)
+    original_image_numpy = np.array(original_image_pillow)  # RGB
+
+    with mp_selfie_segmentation.SelfieSegmentation(model_selection=0) as selfie_segmentation:
+        image_height, image_width, _ = original_image_numpy.shape
+
+        bg_image = np.zeros((image_height, image_width), dtype=np.uint8)
+        bg_image[:] = BG_COLOR
+
+        # Selfie Segmentation
+        results = selfie_segmentation.process(original_image_numpy)
+        condition = np.stack((cv2.blur(results.segmentation_mask, (5, 5)),) * 3, axis=-1) > 0.6
+        segmented_image_numpy = np.where(condition, original_image_numpy, bg_image)
+        segmented_image_pillow = Image.fromarray(segmented_image_numpy)
+
+        # NST
+        segmented_image_tensor = import_from_pil(segmented_image_pillow, device=device, make_batched=True)
+        nst_image_tensor = transformer(segmented_image_tensor)
+        nst_image_pillow = export_to_pil(nst_image_tensor)
+
+        # Combine NST image with original image
+        nst_image_numpy = np.array(nst_image_pillow)
+        output_image_numpy = np.where(condition, nst_image_numpy, original_image_numpy)
+        output_image_pillow = Image.fromarray(output_image_numpy)
+
+        # Output
+        response_image = BytesIO()
+        output_image_pillow.save(response_image, 'JPEG')
+        response_image.seek(0)
+
+        return StreamingResponse(response_image, media_type='image/jpeg')
 
 
 @app.get('/')
